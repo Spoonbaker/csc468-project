361c44a2c1ff9da75ccb3953eabe514e9d9372f9
Spoonbaker <47164123+Spoonbaker@users.noreply.github.com>
2025-02-20 19:20:27 -0500

    root: cloudlab changes
    - use default image; this should make startup faster
    - install docker and docker-compose with apt
    - install lix

--- profile.py
+++ profile.py
@@ -6,12 +6,13 @@ request = portal.context.makeRequestRSpec()
 
 # Life is too short for VMs
 node = request.RawPC("node")
-node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+# node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
 node.routable_control_ip = "true"
 
-node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update"))
-# node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt install -y apache2"))
-# node.addService(rspec.Execute(shell="/bin/sh", command='sudo systemctl status apache2'))
+node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update; sudo apt install -y docker.io docker-compose neovim curl"))
+
+node.addService(rspec.Execute(shell="/bin/sh", command="curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix | sudo sh -s -- install linux --enable-flakes --no-confirm"))
+
 
 # Print the RSpec to the enclosing page.
 portal.context.printRequestRSpec()
