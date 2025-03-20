e37bd913857effbb0a541757fcf6b0ce2bde1ff2
Spoonbaker <47164123+Spoonbaker@users.noreply.github.com>
2025-03-20 16:37:04 -0400

    root: update README, comments in profile.py

--- profile.py
+++ profile.py
@@ -1,9 +1,9 @@
 """
-For Dr. Ngo's CSC468 at West Chester University. The goal is to create a
-cloud-based RSS & Atom feed reader.
+Aggre-gator is a cloud-based RSS & Atom feed reader, created in Dr. Ngo's CSC468
+at West Chester University.
 
 Instructions:
-When it's finished, simply in instantiating this profile should bring everything
+When it's finished, simply instantiating this profile should bring everything
 up. There may be more work to get TLS working.
 """
 
@@ -15,7 +15,12 @@ request = portal.context.makeRequestRSpec()
 
 # Life is too short for VMs
 node = request.RawPC("node")
+
+# We assume that the default image will work.
+# Assuming this lets us avoid re-imaging the disk on every initialization.
 # node.disk_image = "urn:publicid:IDN+emulab.net+image+emulab-ops:UBUNTU22-64-STD"
+
+# Pretty sure we don't need this for `RawPC`s
 node.routable_control_ip = "true"
 
 node.addService(rspec.Execute(shell="/bin/sh", command="sudo apt update; sudo apt install -y docker.io docker-compose neovim curl"))
